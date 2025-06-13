8197701110df0698ff8a2fbbebf4eac91a14d756
Tomas Simanauskas <tomas.simanauskas2@gmail.com>
2025-06-13 20:26:21 +0300

    critical part of the of the page ready

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "Detailing Web Design",
+    "email": "tomas@detailingwebdesign.com",
+    "phoneForTel": "555-779-4407",
+    "phoneFormatted": "(555) 779-4407",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "",
+        "state": "",
+        "zip": "",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.detailingwebdesign.com"
+};
